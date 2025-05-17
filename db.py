c23dad1a1734416a0054fda63842380313aef5af
Punnawat Namkum <6610685247@cn103>
2025-05-17 20:31:03 +0700

    finished

--- db.py
+++ db.py
@@ -6,52 +6,52 @@ import string
 conn = sqlite3.connect("all_books.db")
 cursor = conn.cursor()
 
-# cursor.execute("""
-# CREATE TABLE IF NOT EXISTS books (
-#     key TEXT PRIMARY KEY,
-#     title TEXT,
-#     author TEXT,
-#     first_publish_year INTEGER
-# )
-# """)
-# conn.commit()
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS books (
+    key TEXT PRIMARY KEY,
+    title TEXT,
+    author TEXT,
+    first_publish_year INTEGER
+)
+""")
+conn.commit()
 
-# def fetch_books(query, page=1, limit=100):
-#     url = f"https://openlibrary.org/search.json?q={query}&page={page}&limit={limit}"
-#     response = requests.get(url)
-#     if response.status_code == 200:
-#         return response.json()
-#     else:
-#         print(f"Failed for query={query} page={page}")
-#         return None
+def fetch_books(query, page=1, limit=100):
+    url = f"https://openlibrary.org/search.json?q={query}&page={page}&limit={limit}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Failed for query={query} page={page}")
+        return None
 
-# def save_books(docs):
-#     for book in docs:
-#         key = book.get("key")
-#         title = book.get("title", "")
-#         author = ", ".join(book.get("author_name", [])) if "author_name" in book else "Unknown"
-#         year = book.get("first_publish_year", None)
-#         try:
-#             cursor.execute("""
-#                 INSERT OR IGNORE INTO books (key, title, author, first_publish_year)
-#                 VALUES (?, ?, ?, ?)
-#             """, (key, title, author, year))
-#         except Exception as e:
-#             print("Error inserting:", e)
-#     conn.commit()
+def save_books(docs):
+    for book in docs:
+        key = book.get("key")
+        title = book.get("title", "")
+        author = ", ".join(book.get("author_name", [])) if "author_name" in book else "Unknown"
+        year = book.get("first_publish_year", None)
+        try:
+            cursor.execute("""
+                INSERT OR IGNORE INTO books (key, title, author, first_publish_year)
+                VALUES (?, ?, ?, ?)
+            """, (key, title, author, year))
+        except Exception as e:
+            print("Error inserting:", e)
+    conn.commit()
 
-# queries = list(string.ascii_lowercase) + list("0123456789")
+queries = list(string.ascii_lowercase) + list("0123456789")
 
-# for q in queries:
-#     print(f"Fetching books for query: '{q}'")
-#     for page in range(1, 6):
-#         print(f"  Page {page}")
-#         data = fetch_books(q, page)
-#         if data and "docs" in data:
-#             save_books(data["docs"])
-#         else:
-#             break
-#         time.sleep(1)
+for q in queries:
+    print(f"Fetching books for query: '{q}'")
+    for page in range(1, 6):
+        print(f"  Page {page}")
+        data = fetch_books(q, page)
+        if data and "docs" in data:
+            save_books(data["docs"])
+        else:
+            break
+        time.sleep(1)
 
 cursor.execute("""
 SELECT first_publish_year, COUNT(*) as total_books
